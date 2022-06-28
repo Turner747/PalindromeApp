edee6b1c8ff7eb4999b37785e64ca9666d17beb5
Joshua Turner <joshua.turner2021@gmail.com>
2022-06-28 23:22:58 +1000

    add comments

--- Program.cs
+++ Program.cs
@@ -6,33 +6,36 @@ namespace PalindromeApp
     {
         static void Main(string[] args)
         {
+            // declare required variables
             int originalNumber, tempNumber, remainder, reverseNumber = 0;
 
+            // get input
             Console.WriteLine("\n");
             Console.Write("Enter an integer number >> ");
             originalNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("\n");
 
-            tempNumber = originalNumber;
+            tempNumber = originalNumber; // store original number for later
 
+            // output result
             Console.WriteLine("=====Find Reverse Number=====");
             Console.WriteLine("Original Number: {0}", tempNumber);
             while (originalNumber > 0)
             {
-                remainder = originalNumber % 10;
-                reverseNumber = reverseNumber * 10 + remainder;
-                originalNumber /= 10;
-                Console.WriteLine("{0}", reverseNumber);
+                remainder = originalNumber % 10; // get the last digit
+                reverseNumber = reverseNumber * 10 + remainder; // add decimal place then add last digit
+                originalNumber /= 10; // drop the last digit off the current number
+                Console.WriteLine("{0}", reverseNumber); // out the iteration of the reversed number
             }       
             Console.WriteLine("Reverse Number: {0}", reverseNumber);
             Console.WriteLine("=============================\n");
 
             Console.WriteLine("=============================");
-            if (tempNumber == reverseNumber)
-            {
+            if (tempNumber == reverseNumber) // if the numbers as the same
+            { 
                 Console.WriteLine("{0} is a Palindrone", tempNumber);
             }
-            else
+            else // if they are not the same
             {
                 Console.WriteLine("{0} is not a Palindrone", tempNumber);
             }
